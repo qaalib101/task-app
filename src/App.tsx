75c956319a73649052c3d4551981ab6358b512cb
Qaalib Farah <31248132+qaalib101@users.noreply.github.com>
2025-07-25 17:10:17 -0500

    feat: Replace default UI with Task Manager components
    Removed existing placeholder styles and components (Vite + React) and replaced them with a new Task Manager layout. Integrated `TaskForm`, `FilterBar`, and `TaskList` components, while removing unused CSS files (`App.css` and `index.css`).

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {TaskForm} from "./components/TaskForm.tsx";
+import {FilterBar} from "./components/FilterBar.tsx";
+import {TaskList} from "./components/TaskList.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="max-w-screen-sm mx-auto p-4">
+          <h1 className="text-2xl font-bold mb-4">Task Manager</h1>
+          <TaskForm/>
+          <FilterBar/>
+          <TaskList/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
